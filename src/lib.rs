164e56967d8dd120693d14291448651f693921aa
poly000 <1348292515@qq.com>
2021-08-08 18:40:35 +0800

    It works bad, but still works

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,13 @@ use std::path::PathBuf;
 use image::error::ImageResult;
 
 use image::Frame;
-use image::GenericImage;
 use image::{Rgba, RgbaImage};
 
 use image::codecs::gif::GifEncoder;
 use image::codecs::gif::Repeat;
 
-use image::imageops::crop;
-
-use imageproc::geometric_transformations;
-
-use geometric_transformations::translate;
-use geometric_transformations::warp;
-use geometric_transformations::Interpolation;
-use geometric_transformations::Projection;
+use image::imageops::resize;
+use image::imageops::{overlay, FilterType};
 
 const FRAMES: u32 = 10;
 const RESOLUTION: (u32, u32) = (112, 112);
@@ -39,45 +32,52 @@ const HANDS: SyncLazy<Vec<RgbaImage>> = SyncLazy::new(|| {
 });
 
 pub fn generate(image: RgbaImage) -> ImageResult<impl IntoIterator<Item = Frame>> {
-    let image = translate(&image, (RESOLUTION.0 as i32, RESOLUTION.1 as i32));
     let mut frame = Vec::<Frame>::new();
     for i in 0..FRAMES {
-        let squeeze = if i < FRAMES / 2 { i } else { FRAMES - i } as f32;
+        let squeeze = if i < FRAMES / 2 { i } else { FRAMES - i } as f64;
 
-        let width = squeeze * 0.02 + 0.8;
-        let height = squeeze * 0.05 + 0.8;
+        let width_scale = squeeze * 0.02 + 0.8;
+        let height_scale = squeeze * 0.05 + 0.8;
 
-        let offset_x = (((1.0 - width) * 0.5 + 0.1) * RESOLUTION.0 as f32) as u32;
-        let offset_y = (((1.0 - height) - 0.08) * RESOLUTION.1 as f32) as u32;
+        let width = (width_scale * RESOLUTION.0 as f64) as u32;
+        let height = (height_scale * RESOLUTION.1 as f64) as u32;
 
-        let size_x = RESOLUTION.0 - offset_x;
-        let size_y = RESOLUTION.1 - offset_y;
+        let offset_x = (((1.0 - width_scale) * 0.5 + 0.1) * RESOLUTION.0 as f64) as u32;
+        let offset_y = (((1.0 - height_scale) - 0.08) * RESOLUTION.1 as f64) as u32;
 
-        let mut image = warp(
-            &image,
-            &Projection::scale(width, height),
-            Interpolation::Bicubic,
-            Rgba([0, 0, 0, 0]),
-        ); // rescale image
+        let calucate_then_resize = resize(&image, width, height, FilterType::Lanczos3);
 
-        let image = crop(&mut image, 0, 0, size_x, size_y); // crop
+        let mut resize_then_overlay = RgbaImage::new(RESOLUTION.0, RESOLUTION.1);
 
-        let mut canvas = RgbaImage::new(RESOLUTION.0, RESOLUTION.1);
+        overlay(
+            &mut resize_then_overlay,
+            &calucate_then_resize,
+            offset_x,
+            offset_y,
+        );
 
-        canvas.copy_from(&image, offset_x, offset_y)?;
-        for (pixel_hand, pixel_canvas) in HANDS[i as usize / 2].pixels().zip(canvas.pixels_mut()) {
-            if pixel_hand != &Rgba([0, 0,0,0]) {
+        for (pixel_hand, pixel_canvas) in HANDS[i as usize / 2]
+            .pixels()
+            .zip(resize_then_overlay.pixels_mut())
+        {
+            if pixel_hand != &Rgba([0, 0, 0, 0]) {
                 *pixel_canvas = *pixel_hand;
             }
         }
-        frame.push(Frame::new(canvas));
+
+        let overlay_then_push = resize_then_overlay;
+
+        frame.push(Frame::new(overlay_then_push));
     }
     Ok(frame)
 }
 
-pub fn encode_gif<'a>(frames: impl IntoIterator<Item = Frame>, output: impl Into<PathBuf>) -> ImageResult<()> {
+pub fn encode_gif<'a>(
+    frames: impl IntoIterator<Item = Frame>,
+    output: impl Into<PathBuf>,
+) -> ImageResult<()> {
     let buf = File::create(output.into())?;
-    let mut encoder = GifEncoder::new_with_speed(buf, 1);
+    let mut encoder = GifEncoder::new_with_speed(buf, 3);
     encoder.set_repeat(Repeat::Infinite)?;
     encoder.encode_frames(frames)?;
     Ok(())
