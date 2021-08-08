c29dcea59386003f454995af05049b2c98556b0c
poly000 <1348292515@qq.com>
2021-08-09 02:32:57 +0800

    add bench.rs

---
+++ src/bin/bench.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+use petpet::generate;
+use petpet::encode_gif;
+
+use::petpet::FilterType;
+
+fn main() {
+    let input = env::args().nth(1).expect("input file is required!");
+    let output = env::args().nth(2).expect("output file is required!");
+    for _ in (0..100) {
+        let input_image = image::open(&input).expect("cannot read imput image").to_rgba8();
+        let frames = generate(input_image, FilterType::Triangle).expect("cannot generate frames!");
+        encode_gif(frames, &output).expect("cannot export GIF!");
+    }
+}
