9787a80b312f4eaa44fd17b558729a21b6f6f4c2
poly000 <1348292515@qq.com>
2021-08-08 15:12:18 +0800

    0.1.0

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+#![feature(once_cell)]
+use std::fs::File;
+use std::lazy::SyncLazy;
+use std::path::PathBuf;
+
+use image::error::ImageResult;
+
+use image::Frame;
+use image::GenericImage;
+use image::{Rgba, RgbaImage};
+
+use image::codecs::gif::GifEncoder;
+use image::codecs::gif::Repeat;
+
+use imageproc::geometric_transformations;
+
+use geometric_transformations::translate;
+use geometric_transformations::warp;
+use geometric_transformations::Interpolation;
+use geometric_transformations::Projection;
+
+const FRAMES: u32 = 10;
+const RESOLUTION: (u32, u32) = (112, 112);
+const HANDS: SyncLazy<Vec<RgbaImage>> = SyncLazy::new(|| {
+    (0..5)
+        .map(|num| format!("{}.png", num))
+        .map(|file| {
+            let mut path = PathBuf::from(".");
+            path.push("res");
+            path.push(file);
+
+            image::open(&path)
+                .expect(&format!("Could not load image at {:?}", path))
+                .to_rgba8()
+        })
+        .collect()
+});
+
+pub fn generate(image: RgbaImage) -> ImageResult<impl IntoIterator<Item = Frame>> {
+    let image = translate(&image, (RESOLUTION.0 as i32, RESOLUTION.1 as i32));
+    let mut frame = Vec::<Frame>::new();
+    for i in 0..FRAMES {
+        let squeeze = if i < FRAMES / 2 { i } else { FRAMES - i } as f32;
+        let width = squeeze * 0.02 + 0.8;
+        let height = squeeze * 0.05 + 0.8;
+        let offset_x = ((1.0 - width) * 0.5 + 0.1) * RESOLUTION.0 as f32;
+        let offset_y = ((1.0 - height) - 0.08) * RESOLUTION.1 as f32;
+        let image = warp(
+            &image,
+            &Projection::scale(width, height),
+            Interpolation::Bicubic,
+            Rgba([0, 0, 0, 0]),
+        );
+
+        let mut canvas = RgbaImage::new(RESOLUTION.0, RESOLUTION.1);
+        canvas.copy_from(&image, offset_x as _, offset_y as _)?;
+        canvas.copy_from(&HANDS[i as usize], 0, 0)?;
+        frame.push(Frame::new(canvas));
+    }
+    Ok(frame)
+}
+
+pub fn encode_gif<'a>(frames: impl IntoIterator<Item = Frame>, output: impl Into<PathBuf>) -> ImageResult<()> {
+    let buf = File::create(output.into())?;
+    let mut encoder = GifEncoder::new_with_speed(buf, 20);
+    encoder.encode_frames(frames)?;
+    encoder.set_repeat(Repeat::Infinite)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
+use petpet::generate;
+use petpet::encode_gif;
+
 fn main() {
-    println!("Hello, world!");
+    let input = env::args().nth(1).expect("input file is required!");
+    let output = env::args().nth(2).expect("output file is required!");
+
+    let input_image = image::open(&input).expect("cannot read imput image").to_rgba8();
+
+    let frames = generate(input_image).expect("cannot generate frames!");
+
+    encode_gif(frames, output).expect("cannot export GIF!");
 }
