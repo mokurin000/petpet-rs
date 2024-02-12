708a3b0a1af1694bec7e874b734c5a439ca8109b
poly000 <1348292515@qq.com>
2024-02-13 02:42:10 +0800

    enhance: lazily encode frames

--- src/lib.rs
+++ src/lib.rs
@@ -36,46 +36,45 @@ pub fn generate(
     image: RgbaImage,
     filter: FilterType,
 ) -> ImageResult<impl IntoIterator<Item = Frame>> {
-    let mut frames = Vec::<Frame>::new();
+    Ok((0..FRAMES).map(move |num| encode_single_frame(&image, filter, num)))
+}
 
+fn encode_single_frame(image: &RgbaImage, filter: FilterType, num: u32) -> image::Frame {
     let hands = HANDS_RGBA.get_or_init(|| HANDS_WEBP.map(|img| load_hand_webp(img).unwrap()));
 
-    for i in 0..FRAMES {
-        let squeeze = if i < FRAMES / 2 { i } else { FRAMES - i } as f64;
+    let squeeze = if num < FRAMES / 2 { num } else { FRAMES - num } as f64;
 
-        let width_scale = 0.8 + squeeze * 0.02;
-        let height_scale = 0.8 - squeeze * 0.05;
+    let width_scale = 0.8 + squeeze * 0.02;
+    let height_scale = 0.8 - squeeze * 0.05;
 
-        let width = (width_scale * HAND_HEIGHT_WIDTH as f64) as u32;
-        let height = (height_scale * HAND_HEIGHT_WIDTH as f64) as u32;
+    let width = (width_scale * HAND_HEIGHT_WIDTH as f64) as u32;
+    let height = (height_scale * HAND_HEIGHT_WIDTH as f64) as u32;
 
-        let offset_x = (((1.0 - width_scale) * 0.5 + 0.1) * HAND_HEIGHT_WIDTH as f64) as i64;
-        let offset_y = (((1.0 - height_scale) - 0.08) * HAND_HEIGHT_WIDTH as f64) as i64;
+    let offset_x = (((1.0 - width_scale) * 0.5 + 0.1) * HAND_HEIGHT_WIDTH as f64) as i64;
+    let offset_y = (((1.0 - height_scale) - 0.08) * HAND_HEIGHT_WIDTH as f64) as i64;
 
-        let resized_background = resize(&image, width, height, filter);
+    let resized_background = resize(image, width, height, filter);
 
-        let mut composited_image = RgbaImage::new(HAND_HEIGHT_WIDTH, HAND_HEIGHT_WIDTH);
+    let mut composited_image = RgbaImage::new(HAND_HEIGHT_WIDTH, HAND_HEIGHT_WIDTH);
 
-        overlay(
-            &mut composited_image,
-            &resized_background,
-            offset_x,
-            offset_y,
-        );
+    overlay(
+        &mut composited_image,
+        &resized_background,
+        offset_x,
+        offset_y,
+    );
 
-        let overlay_hand = &hands[i as usize / 2];
-        overlay(&mut composited_image, overlay_hand, 0, 0);
+    let overlay_hand = &hands[num as usize / 2];
+    overlay(&mut composited_image, overlay_hand, 0, 0);
 
-        let frame_with_delay = Frame::from_parts(
-            composited_image,
-            0,
-            0,
-            Delay::from_numer_denom_ms(FRAME_DELAY, 1),
-        );
+    let frame_with_delay = Frame::from_parts(
+        composited_image,
+        0,
+        0,
+        Delay::from_numer_denom_ms(FRAME_DELAY, 1),
+    );
 
-        frames.push(frame_with_delay);
-    }
-    Ok(frames)
+    frame_with_delay
 }
 
 /// Encode Frame to GIF.
